478060cba9df389984308a5e456e345d22da1295
RJiazhen <103310338+RJiazhen@users.noreply.github.com>
2022-10-12 09:50:48 +0800

    feat 完成路由跳转功能

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,11 +1,10 @@
-
 // 引入路由组件
 import Home from '@/pages/Home/index.vue'
 import Search from '@/pages/Search/index.vue'
 
 
 export default [
-  { path: '/', component: Home },
+  { path: '/', component: Home, alias: '/home' },
   { path: '/search', component: Search },
-  { path: '/*', component: Home }
+  { path: '/:pathMatch(.*)', redirect: '/' } // 当上述路径均未匹配时则跳转到/
 ]
