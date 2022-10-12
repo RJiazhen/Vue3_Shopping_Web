2545e9d369b89f07992c2f6c286af686a70ea37b
RJiazhen <103310338+RJiazhen@users.noreply.github.com>
2022-10-12 09:41:43 +0800

    init 引入Element Icon和初始化css

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,15 @@ import App from './App.vue'
 
 import router from '@/router'
 
-// import './assets/main.css'
+import './assets/reset.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
+
+// 挂载elementIcon到app上
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
