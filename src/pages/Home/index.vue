4c9716c3dc76962c305fddb05f1cbb808528b6e2
RJiazhen <103310338+RJiazhen@users.noreply.github.com>
2022-10-17 22:19:54 +0800

    refactor 修改Home子组件引入为相对路径

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -14,13 +14,13 @@
 </template>
 <script lang="ts">
 // 引入组件
-import TypeNav from "@/pages/Home/TypeNav/index.vue"
-import ListContainer from "@/pages/Home/ListContainer/index.vue";
-import Recommend from "@/pages/Home/Recommend/index.vue";
-import Rank from "@/pages/Home/Rank/index.vue";
-import Like from "@/pages/Home/Like/index.vue";
-import Floor from "@/pages/Home/Floor/index.vue";
-import Brand from "@/pages/Home/Brand/index.vue";
+import TypeNav from "./TypeNav/index.vue"
+import ListContainer from "./ListContainer/index.vue";
+import Recommend from "./Recommend/index.vue";
+import Rank from "./Rank/index.vue";
+import Like from "./Like/index.vue";
+import Floor from "./Floor/index.vue";
+import Brand from "./Brand/index.vue";
 
 export default {
   name: "Home",
