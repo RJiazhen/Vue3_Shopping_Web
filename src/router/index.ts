fc4b0fca3c045e22d01b47c174057d9a772872e1
RJiazhen <103310338+RJiazhen@users.noreply.github.com>
2022-11-12 21:57:18 +0800

    feat 完成所有路由跳转时页面统一回到顶部的功能

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,9 @@ import routes from '@/router/routes'
 const router = createRouter({
   history: createWebHistory(),
   routes: routes,
+  scrollBehavior(to, from, savedPositoin) {
+    return { top: 0 }
+  }
 })
 
 export default router
