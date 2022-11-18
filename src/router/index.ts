438fa6d63970d6e895613a84a8a87187bf89624c
RJiazhen <103310338+RJiazhen@users.noreply.github.com>
2022-11-18 15:55:47 +0800

    fix 修复登录后仍能跳转到登录页面的bug

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,26 @@
 // 引入Vue-router
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from '@/router/routes'
+import {useUser} from "@/stores/user"
 
 const router = createRouter({
   history: createWebHistory(),
   routes: routes,
   scrollBehavior(to, from, savedPositoin) {
     return { top: 0 }
+  },
+})
+
+// 路由守卫，每次路由跳转时都会执行
+router.beforeEach((to, from)=>{
+  const user = useUser()
+  let token = user.token
+  let userInfo = user.userInfo
+  if (token) {
+    if(to.name=='login'){
+      return {name:'home'}
+    }
+    else{}
   }
 })
 
